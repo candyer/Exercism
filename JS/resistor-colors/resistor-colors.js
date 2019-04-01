ad0c213187bd03907efa85c79b6377d43aff418d
c <candyer@users.noreply.github.com>
2019-04-01 13:07:25 +0200

    Resistor Colors

---
+++ JS/resistor-colors/resistor-colors.js
@@ -0,0 +1,3 @@
+export const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+export const colorCode = (color) => COLORS.indexOf(color)
+export const value = (colors) => colorCode(colors[0]) * 10 + colorCode(colors[1])
